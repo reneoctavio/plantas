711e69eb02e764683f1b76e20d9a4ed3b3f629fb
René Octavio Queiroz Dias <reneoctavio@gmail.com>
2019-05-19 11:30:32 -0300

    Updated downloader
    No need to download URL files anymore. The script will download directly.

--- download_plantas50.py
+++ download_plantas50.py
@@ -8,11 +8,18 @@ import os
 import sys
 import tarfile
 import time
-import urllib.request
+
+try:
+    import urllib.request as urlrequest
+except ImportError:
+    import urllib as urlrequest
 
 # Plantas50.tar sha256 hash
-P50_SHA256 = "17b35c91e3954735aa59f36555d7162058f3ff538cbd17654eb0fdaf349041e0"
+P50_SHA256 = "af66a7625fcbdc1082ea197c2d4fb6514d362c4e04772bfc06bc7a0f5435a550"
 
+# download_file_list.tar.bz2 sha256 hash and download URL
+DLF_SHA256 = "aacf87348804aa3ff6b9afab2ed8f04cbcaac8433e2d7f09c889440b0fc7505e"
+DLF_URL = "https://github.com/reneoctavio/plantas/raw/master/download_file_list.tar.bz2"
 
 def _check_hash_sha256(file, sha256hash, buffer=65536):
     """Check the hash of a file (SHA256)
@@ -60,37 +67,95 @@ def _progress(count, block_size, total_size):
     downloaded_size = float(count * block_size) / 1024. / 1024. #MB
     velocity = downloaded_size * 1024. / float(elapsed_time) # kB/s
     progress = downloaded_size * 100. / (float(total_size) / 1024. / 1024) # %
+    if progress > 100.:
+        progress = 100.
     sys.stdout.write('\r>>> Downloading %.1f%%, %.lfMB, %.lfkB/s, %s' % \
         (progress, downloaded_size, velocity, elapsed_time_str))
     sys.stdout.flush()
 
 
-def _download(tarball, url, filepath):
+def _download(url, filepath):
     """Helper function that will download a Plantas50 tarball
 
     Args:
-        tarball: The name of the tarball
         url: The URL of the tarball
         filepath: Path to the downloaded tarball
     """
-    filepath, _ = urllib.request.urlretrieve(url, filepath, _progress)
+    filepath, _ = urlrequest.urlretrieve(url, filepath, _progress)
     statinfo = os.stat(filepath)
-    print('\nDownloaded', tarball, statinfo.st_size, 'bytes.')
+    print('\nDownloaded', statinfo.st_size, 'bytes.')
 
-
-def download_and_merge(tarball_url_list, db_dir):
-    """This function will download all tarballs and merge them
+def _remove_tarballs(tarball_url_list, db_dir):
+    """Helper function that will remove the splitted tarballs
 
     Args:
         tarball_url_list: A file containing the tarball name, URL, SHA256 hash
+        db_dir: The directory where the tarballs are
+    """
+    with open(tarball_url_list) as f:
+        for line in f.readlines():
+            tarball, _, _ = line.split()
+            tarball_path = os.path.join(db_dir, tarball)
+            if os.path.isfile(tarball_path):
+                os.remove(tarball_path)
+
+def _extract_plantas50(tarball_url_list, db_dir):
+    """Helper function will extract the Plantas50.tar
+
+    Args:
+        tarball_url_list: A file containing the tarball name, URL, SHA256 hash
+        db_dir: The directory where the tarballs are
+    """
+    print("Plantas50.tar is consistent. Extracting...")
+    _remove_tarballs(tarball_url_list, db_dir)
+    tarball_path = os.path.join(db_dir, 'Plantas50.tar')
+    tarball = tarfile.open(tarball_path, 'r:')
+    tarball.extractall(db_dir)
+    os.remove(tarball_path)
+    print("Plantas50 extracted. Ready!")
+
+def _extract_tar_files_list(fileslistpath, db_dir):
+    """Helper function will extract the download_file_list.tar.bz2
+
+    Args:
+        fileslistpath: Path to download_file_list.tar.bz2
+        db_dir: The directory where the tarballs are
+    """
+    tarball = tarfile.open(fileslistpath, 'r:bz2')
+    tarball.extractall(db_dir)
+    os.remove(fileslistpath)
+
+def download_and_merge(db_dir):
+    """This function will download all tarballs and merge them
+
+    Args:
         db_dir: The directory that the dataset will be saved
     """
+    # Download download_file_list.tar.bz2
+    fileslistpath = os.path.join(db_dir, 'download_file_list.tar.bz2')
+    if os.path.isfile(fileslistpath):
+        print("download_file_list.tar.bz2 already downloaded. Checking consistency...")
+        if _check_hash_sha256(fileslistpath, DLF_SHA256):
+            _extract_tar_files_list(fileslistpath, db_dir)
+        else:
+            print("download_file_list.tar.bz2 is corrupted. Re-downloading...")
+            os.remove(fileslistpath)
+            _download(DLF_URL, fileslistpath)
+            _extract_tar_files_list(fileslistpath, db_dir)
+    else:
+        print("download_file_list.tar.bz2 does not exists. Downloading...")
+        _download(DLF_URL, fileslistpath)
+        _extract_tar_files_list(fileslistpath, db_dir)
+    tarball_url_list = os.path.join(db_dir, 'tar_url_hash.txt')
+
     # Verify if Plantas50 exists and its consistency
     file = os.path.join(db_dir, 'Plantas50.tar')
     if os.path.isfile(file):
         print("Plantas50.tar already downloaded. Checking consistency...")
         if _check_hash_sha256(file, P50_SHA256):
-            print("Plantas50.tar is consistent. Ready!")
+            _extract_plantas50(tarball_url_list, db_dir)
+            if os.path.isfile(tarball_url_list):
+                os.remove(tarball_url_list)
             return
         else:
             print("Plantas50.tar is corrupted. Re-downloading...")
@@ -110,14 +175,14 @@ def download_and_merge(tarball_url_list, db_dir):
                 else:
                     print(tarball + " is corrupted. Re-downloading...")
                     os.remove(tarball_path)
-                    _download(tarball, url, tarball_path)
+                    _download(url, tarball_path)
                     print("Checking consistency...")
                     assert (_check_hash_sha256(tarball_path, sha256)), \
                     "Downloaded file is corrupted. \
                     Try again later or contact owner"
             else:
                 print("Downloading " + tarball + " ...")
-                _download(tarball, url, tarball_path)
+                _download(url, tarball_path)
                 print("Checking consistency...")
                 assert (_check_hash_sha256(tarball_path, sha256)), \
                 "Downloaded file is corrupted. Try again later or contact owner"
@@ -125,37 +190,33 @@ def download_and_merge(tarball_url_list, db_dir):
     # Merge tarballs
     print("Merging tarballs into Plantas50.tar. This should take awhile...")
     tarballs_path = os.path.join(db_dir, 'Plantas50.tar.part')
-    plantas50path = os.path.join(db_dir, 'Plantas50.tar')
 
     if os.name == 'nt':
-        os.system("copy /b " + tarballs_path + "* " + plantas50path)
+        os.system("copy /b " + tarballs_path + "* " + file)
     else:
-        os.system("cat " + tarballs_path + "* > " + plantas50path)
+        os.system("cat " + tarballs_path + "* > " + file)
 
-    if _check_hash_sha256(plantas50path, P50_SHA256):
-        print("Plantas50.tar is consistent. Ready!")
+    if _check_hash_sha256(file, P50_SHA256):
+        _extract_plantas50(tarball_url_list, db_dir)
     else:
         print("Plantas50.tar is corrupted. Re-run the script.")
-        os.remove(plantas50path)
+        os.remove(file)
+
+    if os.path.isfile(tarball_url_list):
+        os.remove(tarball_url_list)
 
 def main(argv):
-    tarball_url_hash_file = os.path.abspath('./files_url_hashes.txt')
     db_dir = os.path.abspath('./')
 
-    if len(argv) == 1:
-        assert (os.path.isfile(tarball_url_hash_file)), "The \
-        files_url_hashes.txt file was not found in the current directory. \
-        Place it here or indicate its path with the command: python \
-        download_plantas50.py files_url_hashes_path dataset_directory"
-    elif len(argv) == 3:
-        tarball_url_hash_file = os.path.abspath(argv[1])
-        db_dir = os.path.abspath(argv[2])
+    if len(argv) == 2:
+        db_dir = os.path.abspath(argv[1])
+        assert (os.path.isdir(db_dir)), "Not a valid directory."
     else:
         print("The command should be python download_plantas50.py \
-        tarball_url_hash_file dataset_directory")
+            dataset_directory")
         return
 
-    download_and_merge(tarball_url_hash_file, db_dir)
+    download_and_merge(db_dir)
 
 if __name__ == "__main__":
     main(sys.argv)
